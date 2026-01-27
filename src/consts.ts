c83bb6e8d840d97d5064047583bca7cfbaad58f7
Emily Phelan <emilyp94.cork@gmail.com>
2026-01-27 19:44:10 +0000

    Update blog name and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Mind the Gael';
+export const SITE_DESCRIPTION = 'Mental Health Awareness for Women in Sport';
